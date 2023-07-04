18cf4a947b7a276c2239b9f8fb39da0a63615054
김희민 <khk8300@naver.com>
2023-07-04 10:28:19 +0900

    임시 페이지용 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import MainLogo from "public/icons/MainLogo.svg";
+import MainLogo from "public/icons/MainLogo2.svg";
 import Link from "next/link";
 
 function Header() {
@@ -8,21 +8,30 @@ function Header() {
     <div className="sticky z-50 bg-white  top-0 p-4 border-b-2 border-[#545454] shadow-xl">
       <div className=" mx-auto flex flex-wrap flex-col md:flex-row  items-center ">
         <div className="flex title-font font-medium items-center  text-gray-900 mb-4 md:mb-0  w-40 max-[360px]:w-28  ">
-          <Image src={MainLogo} alt="mainLogo" />
+          <Link href="/">
+            <Image src={MainLogo} alt="mainLogo" />
+          </Link>
         </div>
 
         <div className="flex md:ml-auto   max-[480px]:flex-col  flex-row  flex-wrap items-center text-base md:self-end self-center justify-center gap-4">
-          <Link href="http://www.naver.com" target="_blank">
+          <Link href="https://cafe.naver.com/kindfiner" target="_blank">
             <div className=" hover:text-gray-900  text-lg font-bold drop-shadow-lg  max-[360px]:text-base ">
               추세선생 네이버카페
             </div>
           </Link>
-          <div className=" hover:text-gray-900 text-lg font-bold drop-shadow-lg  max-[360px]:text-base ">
-            유튜브채널
-          </div>
-          <div className="hover:text-gray-900 text-lg font-bold drop-shadow-lg  max-[360px]:text-base">
-            유안타증권
-          </div>
+          <Link href="https://www.youtube.com/@TV-sy5er" target="_blank">
+            <div className=" hover:text-gray-900 text-lg font-bold drop-shadow-lg  max-[360px]:text-base ">
+              유튜브채널
+            </div>
+          </Link>
+          <Link
+            href="https://www.myasset.com/extern/eventtotal/views/dmstcStock.jsp?mkt=keyword_naverpc_tradar&gclid=CjwKCAjw44mlBhAQEiwAqP3eVtCtdTiIYnP5TPcjoIOwD345q6QyK6oxlOKGBnubcxeiRki_hkoOOxoCEd0QAvD_BwE"
+            target="_blank"
+          >
+            <div className="hover:text-gray-900 text-lg font-bold drop-shadow-lg  max-[360px]:text-base">
+              유안타증권
+            </div>
+          </Link>
         </div>
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import HTSImage from "public/icons/HTSImage.svg";
 import MTSImage from "public/icons/MtsImage.svg";
 import WTSImage from "public/icons/WtsImage.svg";
+import Link from "next/link";
 
 function Main() {
   return (
@@ -16,12 +17,12 @@ function Main() {
               추세선생- <span className="text-black">HTS</span>
             </div>
             <div className=" self-center ">
-              <button className="flex text-xl items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-gray-200 rounded ">
-                다운로드
+              <button className="flex text-xl items-center bg-gray-400 text-white font-bold border-0 py-1 px-3 focus:outline-none  rounded  cursor-not-allowed disabled">
+                준비중
               </button>
             </div>
           </div>
-          <div className=" pl-0 xl:pl-16 font-bold text-lg  w-max[360px]: gap-4 flex flex-col self-center justify-center w-full text-center xl:text-left  max-[360px]:text-base  max-[360px]:font-extrabold mb-2 ">
+          <div className=" pl-0 xl:pl-16 font-bold text-lg  w-max[360px]: gap-4 flex flex-col self-center justify-center w-full text-center xl:text-left  max-[360px]:text-base  max-[360px]:font-extrabold mb-2 xl:pr-12 pr:0 ">
             <div>테마의 추세와 테마종목별 추세현황을 한눈에</div>
             <div>자동추세선을 이용한 기술적분석 자동화 차트솔루션</div>
 
@@ -41,12 +42,14 @@ function Main() {
               추세선생- <span className="text-black">WTS</span>
             </div>
             <div className=" self-center ">
-              <button className="flex text-xl items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-gray-200 rounded ">
-                다운로드
-              </button>
+              <Link href="https://www.추세선생.com" target="_blank">
+                <button className="flex text-xl items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-blue-500 rounded">
+                  다운로드
+                </button>
+              </Link>
             </div>
           </div>
-          <div className=" pl-0 xl:pl-16 font-bold text-lg  w-max[360px]: gap-4 flex flex-col self-center justify-center w-full text-center xl:text-left  max-[360px]:text-base  max-[360px]:font-extrabold mb-2 ">
+          <div className=" pl-0 xl:pl-16 font-bold text-lg  w-max[360px]: gap-4 flex flex-col self-center justify-center w-full text-center xl:text-left  max-[360px]:text-base  max-[360px]:font-extrabold mb-2 xl:pr-8 pr:0 ">
             <div>글로벌 차트프로그램 트레이딩뷰 무료배포용 웹페이지</div>
             <div>종목별 전문 분석보고서와 ETF 편입 정보 제공</div>
 
@@ -66,8 +69,8 @@ function Main() {
               추세선생- <span className="text-black">MTS</span>
             </div>
             <div className=" self-center ">
-              <button className="flex text-xl items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-gray-200 rounded ">
-                다운로드
+              <button className="flex text-xl items-center bg-gray-400 text-white font-bold border-0 py-1 px-3 focus:outline-none  rounded  cursor-not-allowed disabled">
+                준비중
               </button>
             </div>
           </div>
@@ -88,93 +91,3 @@ function Main() {
 }
 
 export default Main;
-
-// "use client";
-
-// import React from "react";
-// import HTSImage from "public/icons/HTSImage.svg";
-
-// import Image from "next/image";
-
-// function Main() {
-//   return (
-//     <div classNameName="flex-grow ">
-//       <div className="flex flex-col w-full h-full justify-between px-28 mt-8 bg-main-texture bg-inherit bg-cover">
-//         <div className="container px-5 py-24 mx-auto flex flex-wrap">
-//           <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-//             <div className="flex flex-row flex-none w-full sm:p-4 px-4 mb-6">
-//               <h1 className="title-font font-medium text-xl mb-2 text-gray-900 w-full">
-//                 <div className="flex w-full  text-4xl font-bold text-[#545454] flex-none">
-//                   추세선생- <span className="text-black">HTS</span>
-//                 </div>
-//               </h1>
-//               <div className="leading-relaxed flex-none self-center justify-end">
-//                 <button className="flex text-lg items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-gray-200 rounded ">
-//                   다운로드
-//                 </button>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-6 pl-16 font-bold text-lg">
-//               <div className=" ">
-//                 테마의 추세와 테마종목별 추세현황을 한눈에
-//               </div>
-//               <div className=" ">
-//                 자동추세선을 이용한 기술적분석 자동화 차트솔루션
-//               </div>
-//               <div className=" ">분할매매 정보와 전략보고서를 실시간 제공</div>
-//               <div className=" ">
-//                 상승각도를 판정, 기준각 유효성 평가 시스템
-//               </div>
-//             </div>
-//           </div>
-//           <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-//             <Image
-//               src={HTSImage}
-//               className="object-cover object-center w-full h-full"
-//               alt="stats"
-//             />
-//           </div>
-//         </div>
-
-//         {/* //////////////////////////////// */}
-//         <div className="container px-5 py-24 mx-auto flex flex-wrap">
-//           <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-//             <div className="w-full sm:p-4 px-4 mb-6">
-//               <h1 className="title-font font-medium text-xl mb-2 text-gray-900">
-//                 <div className="flex w-full  text-4xl font-bold text-[#545454]">
-//                   추세선생- <span className="text-black">HTS</span>
-//                 </div>
-//               </h1>
-//               <div className="leading-relaxed">
-//                 <button className="flex text-lg items-center bg-[#54AC35] text-white font-bold border-0 py-1  px-3 focus:outline-none hover:bg-gray-200 rounded ">
-//                   다운로드
-//                 </button>
-//               </div>
-//             </div>
-//             <div className="flex flex-col gap-6">
-//               <div className=" ">
-//                 테마의 추세와 테마종목별 추세현황을 한눈에
-//               </div>
-//               <div className=" ">
-//                 자동추세선을 이용한 기술적분석 자동화 차트솔루션
-//               </div>
-//               <div className=" ">분할매매 정보와 전략보고서를 실시간 제공</div>
-//               <div className=" ">
-//                 상승각도를 판정, 기준각 유효성 평가 시스템
-//               </div>
-//             </div>
-//           </div>
-//           <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-//             <Image
-//               src={HTSImage}
-//               className="object-cover object-center w-full h-full"
-//               alt="stats"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Main;
