2b1a5f581c66e27f692f4bf2325b41e8f4a7e696
김희민 <khk8300@naver.com>
2023-07-07 16:01:37 +0900

    favicon 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
